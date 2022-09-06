c1903ff165ef2dbf42be4a02732d9c9d5d4a7797
druha <druha@Osu>
2022-09-06 16:58:15 +0530

    Bubble_sort

--- eclipse-workspace/sorting_1/src/sorting_1/bubblesort.java
+++ eclipse-workspace/sorting_1/src/sorting_1/bubblesort.java
@@ -1,10 +1,34 @@
 package sorting_1;
-
+import java.util.*;
 public class bubblesort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	   static void bubbleSort(int[] arr) {  
+	      int n = arr.length;  
+	      int temp = 0;  
+	      for(int i = 0; i < n; i++){  
+	         for(int j = 1; j < (n-i); j++){  
+	            if(arr[j-1] > arr[j]){  
+	               //swap elements  
+	               temp = arr[j-1];  
+	               arr[j-1] = arr[j];  
+	               arr[j] = temp;  
+	            }          
+	         } }
+	      
+public class bubblesort {
+	public void main(String[] args) {
+		 int arr[] = {21,60,32,01,41,34,5};
+	      System.out.println("Before Bubble Sort");  
+	      for(int i = 0; i < arr.length; i++){  
+	         System.out.print(arr[i] + " ");  
+	      }  
+	      System.out.println();  
+	      bubbleSort(arr);
+	      System.out.println("After Bubble Sort");  
+	      for(int i = 0; i < arr.length; i++){  
+	         System.out.print(arr[i] + " ");  
+	      }  
 	}
-
+		
+	}
+	   }
 }
